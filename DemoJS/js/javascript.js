7aa4594b4ff0649e31ce3a53706513ae5c295e2e
chinhnguyen93 <ndchinh93@gmail.com>
2016-10-07 15:04:16 +0700

    Quiz
    1

---
+++ DemoJS/js/javascript.js
@@ -0,0 +1,200 @@
+
+		var cau1 = {
+				cauhoi: "Có một tàu điện đi về hướng nam. Gió hướng tây bắc. Vậy khói từ con tàu sẽ theo hướng nào?",
+				ans1: "Đông",
+				ans2: "Tây",
+				ans3: "Bắc",
+				ans4: "Không hướng nào",
+				da: 4
+				//D
+			};
+
+		var cau2={
+				cauhoi: "Làm thế nào để không đụng phải ngón tay khi bạn đập búa vào một cái móng tay?",
+				ans1: "Cầm búa bằng cả 2 tay",
+				ans2: "Cầm búa bằng tay trái",
+				ans3: "Cầm búa bằng tay phải",
+				ans4: "Cầm búa bằng chân",
+				da: 1
+				//A
+			};
+
+		var cau3={
+				cauhoi: "Có anh chàng dừng xe ở trước cửa 1 Nhà nghỉ nọ, vẻ như đang chờ ai đó. Chợt có một bà bầu đi ngang qua, anh ta liền hỏi: Chị làm ơn coi hộ mấy giờ?. Chị kia đáp nhanh: Mười tám tháng. Anh chàng vội cảm ơn rồi lẩm bẩm 'Mình đến sớm 20phút rồi, lấy phòng lên nằm đợi vậy. Bạn cho biết lúc đó đồng hồ trên tay anh kia chỉ mấy giờ?",
+				ans1: "5h30'",
+				ans2: "15h30'",
+				ans3: "10h00'",
+				ans4: "18h30'",
+				da: 1
+				//A
+			};
+
+		var cau4={
+				cauhoi: "Nếu bạn nhìn thấy con chim đang đậu trên nhánh cây, làm sao để lấy nhánh cây mà không làm động con chim?",
+				ans1: "Bắt chim bỏ ra ngoài",
+				ans2: "Đợi chim bay đi.",
+				ans3: "Ru chim ngủ rồi lấy",
+				ans4: "Cứ đến mà lấy",
+				da: 4
+				//D
+			};
+
+		var cau5={
+				cauhoi: "Từ gì mà 100% người Việt Nam đều phát âm sai?",
+				ans1: "Đúng",
+				ans2: "Sai",
+				ans3: "Nóng",
+				ans4: "Lạnh",
+				da: 2
+				//B
+			};
+
+		var cau6={
+				cauhoi: "Miệng rộng lớn nhưng không nói một từ, là con gì?",
+				ans1: "Con cá voi",
+				ans2: "Con khỉ đột",
+				ans3: "Con sông",
+				ans4: "Con voi",
+				da: 3
+				//C
+			};
+
+		var cau7={
+				cauhoi: "Có bao nhiêu chữ C trong câu sau: Cơm, canh, cá, tất cả em đều thích?",
+				ans1: "1",
+				ans2: "3",
+				ans3: "4",
+				ans4: "5",
+				da: 1
+				//A
+			};
+
+		var cau8={
+				cauhoi: "Loại nước giải khát nào chứa Canxi và Sắt?",
+				ans1: "CoCa",
+				ans2: "Pepsi",
+				ans3: "Cafe",
+				ans4: "Sinh tố",
+				da: 3
+				//C
+			};
+
+		var cau9={
+				cauhoi: "Cái gì bạn không mượn mà trả?",
+				ans1: "Tiền",
+				ans2: "Lời cảm ơn",
+				ans3: "Tình",
+				ans4: "Không có thứ gì",
+				da: 2
+				//B
+			};
+
+		var cau10={
+				cauhoi: "Vào lúc nửa đêm đồng hồ bất chợt gõ 13 tiếng, chuyện gì xảy ra?",
+				ans1: "Có ma",
+				ans2: "Chuyện xấu sẽ đến",
+				ans3: "Ngày tận thế",
+				ans4: "Mang đồng hồ đi sửa",
+				da: 4
+				//D
+			};
+
+		var cau11={
+				cauhoi: "Cái gì luôn đi đến mà không bao giờ đến nơi?",
+				ans1: "Cơn gió",
+				ans2: "Tình yêu",
+				ans3: "Ngày mai",
+				ans4: "Không biết là cái gì",
+				da: 3
+				//C
+			};
+
+		var cau12={
+				cauhoi: "Tìm điểm sai trong câu: Dưới ánh nắng sương long lanh triệu cành hồng khoe sắc thắm.",
+				ans1: "Ánh nắng sương",
+				ans2: "Triệu cành hồng",
+				ans3: "Khoe sắc thắm",
+				ans4: "Sương long lanh",
+				da: 3
+				//C
+			};
+
+		var cau13={
+				cauhoi: "8 chia 4 = ?",
+				ans1: "1/4",
+				ans2: "1/2",
+				ans3: "3/4",
+				ans4: "4/3",
+				da: 4
+				//D Tứ chia tam
+			};
+
+		var cau14={
+				cauhoi: "Một anh thanh niên đánh 1 bà già hỏi anh ấy mất gì?",
+				ans1: "Mất tiền",
+				ans2: "Mất trí",
+				ans3: "Mất sức",
+				ans4: "Mất dạy",
+				da: 4
+				//D
+			};
+
+		var cau15={
+				cauhoi: "Có 2 người cha và 2 người con cùng chia đều số tiền là 27 nghìn. Hỏi mỗi người được bao nhiều?",
+				ans1: "6,75",
+				ans2: "7",
+				ans3: "7.5",
+				ans4: "9",
+				da: 4
+				//D
+			};
+
+		var cau16={
+				cauhoi: "Có 1 đàn chuột điếc đi ngang qua hố cống, hỏi có mấy con?",
+				ans1: "12",
+				ans2: "18",
+				ans3: "24",
+				ans4: "30",
+				da: 3
+				//C
+			};
+
+		var cau17={
+				cauhoi: "Cái gì mà đi thì nằm, đứng cũng nằm, nhưng nằm lại đứng?",
+				ans1: "Cái bàn",
+				ans2: "Cái ghế",
+				ans3: "Bàn chân",
+				ans4: "Bàn tay",
+				da: 3
+				//C
+			};
+
+		var cau18={
+				cauhoi: "Một kẻ giết người bị kết án tử hình. Hắn ta phải chọn một trong ba căn phòng: phòng thứ nhất lửa cháy dữ dội, phòng thứ hai đầy những kẻ ám sát đang giương súng, và phòng thứ ba đầy sư tử nhịn đói trong ba năm. Phòng nào an toàn nhất cho hắn?",
+				ans1: "Phòng thứ nhất",
+				ans2: " Phòng thứ hai",
+				ans3: "Phòng thứ ba",
+				ans4: "Không phòng nào",
+				da: 3
+				//C
+			};
+
+		var cau19={
+				cauhoi: "Bố mẹ có 6 người con trai, mỗi người con trai có một đứa em gái. Vậy gia đình đó có mấy người?",
+				ans1: "8",
+				ans2: "9",
+				ans3: "10",
+				ans4: "11",
+				da: 2
+				//B
+			};
+
+		var cau20={
+				cauhoi: "Chồng người da đen, vợ người da trắng vừa sinh một đứa bé, răng của nó màu gì?",
+				ans1: "Không răng",
+				ans2: "Trắng",
+				ans3: "Vàng",
+				ans4: "Đỏ",
+				da: 1
+				//A
+			};
